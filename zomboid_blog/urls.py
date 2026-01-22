c29d93c9634977160b9a81228952ef76867e6939
Blake Nall <blake@crimsonslate.com>
2026-01-21 21:48:47 -0600

    Reorganized templates and added home view

--- zomboid_blog/urls.py
+++ zomboid_blog/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.BlogPostListView.as_view(), name="blogpost list"),
+    path("", views.HomeView.as_view(), name="home"),
+    path(
+        "posts/list/", views.BlogPostListView.as_view(), name="blogpost list"
+    ),
     path(
         "posts/<str:slug>-<int:pk>/",
         views.BlogPostDetailView.as_view(),
--- zomboid_blog/views.py
+++ zomboid_blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, TemplateView
 from django.db.models import QuerySet
 
 from . import mixins, models
@@ -6,11 +6,18 @@ from . import mixins, models
 PUBLIC = models.BlogPost.BlogPostStatus.PUBLIC
 
 
+class HomeView(mixins.HtmxTemplateResponseMixin, TemplateView):
+    content_type = "text/html"
+    http_method_names = ["get"]
+    template_name = "zomboid_blog/home.html"
+
+
 class BlogPostDetailView(mixins.HtmxTemplateResponseMixin, DetailView):
     content_type = "text/html"
     http_method_names = ["get"]
     model = models.BlogPost
     queryset = models.BlogPost.objects.filter(status=PUBLIC)
+    template_name = "zomboid_blog/blogpost/detail.html"
 
 
 class BlogPostListView(mixins.HtmxTemplateResponseMixin, ListView):
@@ -21,6 +28,7 @@ class BlogPostListView(mixins.HtmxTemplateResponseMixin, ListView):
     ordering = "-pub_date"
     paginate_by = 50
     queryset = models.BlogPost.objects.filter(status=PUBLIC)
+    template_name = "zomboid_blog/blogpost/list.html"
 
     def get_queryset(self) -> QuerySet:
         qs = super().get_queryset()
